ef879c3bc96a14022e64d558928935c2166cd29e
TanyTany <avdtatyana@gmail.com>
2021-02-22 20:55:50 +0300

    added .appveyor.yml

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value, max data"))
+                .body("data", equalTo("max data"))
         ;
     }
 }
